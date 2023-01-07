c4752a963230fb62ee0e3cadb81d19bdaa19614b
bed-com-31-19 <bed-com-31-19@unima.ac.mw>
2023-01-08 01:52:16 +0200

    complete sidebar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,17 @@ import React from "react";
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import TopBar from "./scenes/global/TopBar";
+import SideBar from "./scenes/global/SideBar";
+import Dashboard from "./scenes/dashboard/Dashboard";
+// import Team from "./scenes/Team";
+// import Invoice from "./scenes/invoices/Invoice";
+// import Contact from "./scenes/contacts/Contact";
+// import Bar from "./scenes/Bar";
+// import Line from "./scenes/Line";
+// import Pie from "./scenes/Pie";
+// import Calendar from "./scenes/Calendar";
+import { Routes, Route } from "react-router-dom";
+
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -10,9 +21,23 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
+          <SideBar />
           <main className="content">
-            <TopBar/>
-            <h1>Welcome to AdminDashboard</h1>
+            <TopBar />
+
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              {/* <Route path="/team" element={<Team />} /> */}
+              {/* <Route path="/contacts" element={<Contact/>} /> */}
+              {/* <Route path="/invoices" element={<Invoice />} /> */}
+              {/* <Route path="/form" element={<Form />} /> */}
+              {/* <Route path="/bar" element={<Bar />} /> */}
+              {/* <Route path="/pie" element={<Pie/>} /> */}
+              {/* <Route path="/line" element={<Line />} /> */}
+              {/* <Route path="/faq" element={<FAQ/>} /> */}
+              {/* <Route path="/calender" element={<Calendar />} /> */}
+              {/* <Route path="/geography" element={<Geography/>} /> */}
+            </Routes>
           </main>
         </div>
       </ThemeProvider>
