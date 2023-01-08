1c8a07d9c27c3d28d1ef25f72f4ca5351866e5f9
bed-com-31-19 <bed-com-31-19@unima.ac.mw>
2023-01-08 03:27:51 +0200

    adding team

---
+++ src/scenes/team/Team.jsx
@@ -0,0 +1,93 @@
+import React from "react";
+import { DataGrid } from "@mui/x-data-grid";
+import { mockDataTeam } from "../../data/mockData";
+import { tokens } from "../../theme";
+import { Typography, useTheme, Box } from "@mui/material";
+import AdminPanelSettingsOutlined from "@mui/icons-material/AdminPanelSettingsOutlined";
+import LockOpenOutlined from "@mui/icons-material/LockOpenOutlined";
+import SecurityOutlined from "@mui/icons-material/SecurityOutlined";
+import Header from "../../components/Header";
+
+const Team = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const columns = [
+    {
+      field: "id",
+      headerName: "ID",
+    },
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 1,
+      cellClassName: "name-column--cell",
+    },
+    {
+      field: "age",
+      headerName: "Age",
+      type: "number",
+      headerAlign: "left",
+      align: "left",
+    },
+    {
+      field: "phone",
+      headerName: "Phone Number",
+      flex: 1,
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      flex: 1,
+    },
+    {
+      field: "acces",
+      headerName: "Access Level",
+      flex: 1,
+      renderCell: ({ row: { access } }) => {
+        return (
+          <Box
+            width="60%"
+            m="0 auto"
+            p="5px"
+            display="flex"
+            justifyContent="center"
+            backgroundColor={
+              access === "admin"
+                ? colors.greenAccent[600]
+                : colors.greenAccent[700]
+            }
+            borderRadius="4px"
+          >
+            {access === "admin" && <AdminPanelSettingsOutlined />}
+            {access === "manager" && <SecurityOutlined />}
+            {access === "user" && <LockOpenOutlined />}
+            <Typography color={colors.grey[100]} sx={{ ml: "5px" }}>
+              {access}
+            </Typography>
+          </Box>
+        );
+      },
+    },
+  ];
+  return (
+    <Box m="20px">
+      <Header title="Team" subtitle="Managing the Team Members" />
+          <Box m="40px 0 0 0" height="75vh"
+              sx={{
+                  "& .MuiDataGrid-root": {
+                 border: "none"
+                  },
+                  "& .MuiDataGrid-cell": {
+                      borderBottom: "none",
+                      borderColor: colors.grey[300],
+                  },
+                  
+                 
+         }} >
+        <DataGrid rows={mockDataTeam} columns={columns} />
+      </Box>
+    </Box>
+  );
+};
+
+export default Team;
