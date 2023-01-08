e4f1e6f22d4fc99186f477840524fb7901730d79
bed-com-31-19 <bed-com-31-19@unima.ac.mw>
2023-01-08 04:20:10 +0200

    checkbox selection

--- src/scenes/team/Team.jsx
+++ src/scenes/team/Team.jsx
@@ -1,21 +1,17 @@
-import React from "react";
+import { Box, Typography, useTheme } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
-import { mockDataTeam } from "../../data/mockData";
 import { tokens } from "../../theme";
-import { Typography, useTheme, Box } from "@mui/material";
-import AdminPanelSettingsOutlined from "@mui/icons-material/AdminPanelSettingsOutlined";
-import LockOpenOutlined from "@mui/icons-material/LockOpenOutlined";
-import SecurityOutlined from "@mui/icons-material/SecurityOutlined";
+import { mockDataTeam } from "../../data/mockData";
+import AdminPanelSettingsOutlinedIcon from "@mui/icons-material/AdminPanelSettingsOutlined";
+import LockOpenOutlinedIcon from "@mui/icons-material/LockOpenOutlined";
+import SecurityOutlinedIcon from "@mui/icons-material/SecurityOutlined";
 import Header from "../../components/Header";
 
 const Team = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   const columns = [
-    {
-      field: "id",
-      headerName: "ID",
-    },
+    { field: "id", headerName: "ID" },
     {
       field: "name",
       headerName: "Name",
@@ -40,7 +36,7 @@ const Team = () => {
       flex: 1,
     },
     {
-      field: "acces",
+      field: "accessLevel",
       headerName: "Access Level",
       flex: 1,
       renderCell: ({ row: { access } }) => {
@@ -54,13 +50,15 @@ const Team = () => {
             backgroundColor={
               access === "admin"
                 ? colors.greenAccent[600]
+                : access === "manager"
+                ? colors.greenAccent[700]
                 : colors.greenAccent[700]
             }
             borderRadius="4px"
           >
-            {access === "admin" && <AdminPanelSettingsOutlined />}
-            {access === "manager" && <SecurityOutlined />}
-            {access === "user" && <LockOpenOutlined />}
+            {access === "admin" && <AdminPanelSettingsOutlinedIcon />}
+            {access === "manager" && <SecurityOutlinedIcon />}
+            {access === "user" && <LockOpenOutlinedIcon />}
             <Typography color={colors.grey[100]} sx={{ ml: "5px" }}>
               {access}
             </Typography>
@@ -69,22 +67,40 @@ const Team = () => {
       },
     },
   ];
+
   return (
     <Box m="20px">
-      <Header title="Team" subtitle="Managing the Team Members" />
-          <Box m="40px 0 0 0" height="75vh"
-              sx={{
-                  "& .MuiDataGrid-root": {
-                 border: "none"
-                  },
-                  "& .MuiDataGrid-cell": {
-                      borderBottom: "none",
-                      borderColor: colors.grey[300],
-                  },
-                  
-                 
-         }} >
-        <DataGrid rows={mockDataTeam} columns={columns} />
+      <Header title="TEAM" subtitle="Managing the Team Members" />
+      <Box
+        m="40px 0 0 0"
+        height="75vh"
+        sx={{
+          "& .MuiDataGrid-root": {
+            border: "none",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none",
+          },
+          "& .name-column--cell": {
+            color: colors.greenAccent[300],
+          },
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: colors.blueAccent[700],
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: colors.primary[400],
+          },
+          "& .MuiDataGrid-footerContainer": {
+            borderTop: "none",
+            backgroundColor: colors.blueAccent[700],
+          },
+          "& .MuiCheckbox-root": {
+            color: `${colors.greenAccent[200]} !important`,
+          },
+        }}
+      >
+        <DataGrid checkboxSelection rows={mockDataTeam} columns={columns} />
       </Box>
     </Box>
   );
