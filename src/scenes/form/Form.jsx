1f9193a97f6e448353da51bc0763ecc9680bced5
bed-com-31-19 <bed-com-31-19@unima.ac.mw>
2023-01-09 04:17:30 +0200

    complete validation form

--- src/scenes/form/Form.jsx
+++ src/scenes/form/Form.jsx
@@ -1,9 +1,24 @@
-import React from "react";
 import { Box, Button, TextField } from "@mui/material";
 import { Formik } from "formik";
 import * as yup from "yup";
-import { useMediaQuery } from "@mui/material";
+import useMediaQuery from "@mui/material/useMediaQuery";
 import Header from "../../components/Header";
+
+
+const phoneRegExp =
+  /^((\+[1-9]{1,4}[ -]?)|(\([0-9]{2,3}\)[ -]?)|([0-9]{2,4})[ -]?)*?[0-9]{3,4}[ -]?[0-9]{3,4}$/;
+
+const checkoutSchema = yup.object().shape({
+  firstName: yup.string().required("required"),
+  lastName: yup.string().required("required"),
+  email: yup.string().email("invalid email").required("required"),
+  contact: yup
+    .string()
+    .matches(phoneRegExp, "Phone number is not valid")
+    .required("required"),
+  address1: yup.string().required("required"),
+  address2: yup.string().required("required"),
+});
 const initialValues = {
   firstName: "",
   lastName: "",
@@ -14,30 +29,21 @@ const initialValues = {
 };
 
 
-const phoneRegExp =
-  /^((\+[1-9]{1,4}[ -]?)|(\([0-9]{2,3}\)[ -]?)|([0-9]{2,4})[ -]?)*?[0-9]{3,4}[ -]?[0-9]{3,4}$/;
-
-const userSchema = yup.object().shape({
-  firstName: yup.string().required("required"),
-  lastName: yup.string().required("required"),
-  email: yup.string().email("Invalid email").required("required"),
-  contact: yup.string().matches(phoneRegExp, "Phone number is not valid").required("required"),
-  address1: yup.string().required("required"),
-  address2: yup.string().required("required"),
-});
-
 const Form = () => {
-  const isNonMobile = useMediaQuery("min-width: 600px");
+  const isNonMobile = useMediaQuery("(min-width:600px)");
+
   const handleFormSubmit = (values) => {
     console.log(values);
   };
+
   return (
     <Box m="20px">
       <Header title="CREATE USER" subtitle="Create a New User Profile" />
+
       <Formik
         onSubmit={handleFormSubmit}
         initialValues={initialValues}
-        validationSchema={userSchema}
+        validationSchema={checkoutSchema}
       >
         {({
           values,
@@ -51,11 +57,9 @@ const Form = () => {
             <Box
               display="grid"
               gap="30px"
-              gridTemplateColumns="repeate(4, minmax(0, 1fr))"
+              gridTemplateColumns="repeat(4, minmax(0, 1fr))"
               sx={{
-                "& > div": {
-                  gridColumn: isNonMobile ? undefined : "span 2",
-                },
+                "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
               }}
             >
               <TextField
@@ -71,7 +75,6 @@ const Form = () => {
                 helperText={touched.firstName && errors.firstName}
                 sx={{ gridColumn: "span 2" }}
               />
-
               <TextField
                 fullWidth
                 variant="filled"
@@ -102,7 +105,7 @@ const Form = () => {
                 fullWidth
                 variant="filled"
                 type="text"
-                label="Contact"
+                label="Contact Number"
                 onBlur={handleBlur}
                 onChange={handleChange}
                 value={values.contact}
@@ -137,6 +140,11 @@ const Form = () => {
                 helperText={touched.address2 && errors.address2}
                 sx={{ gridColumn: "span 4" }}
               />
+            </Box>
+            <Box display="flex" justifyContent="end" mt="20px">
+              <Button type="submit" color="secondary" variant="contained">
+                Create New User
+              </Button>
             </Box>
           </form>
         )}
