6c01643f108664f08832de085a8acbfaf7ead70b
bed-com-31-19 <bed-com-31-19@unima.ac.mw>
2023-01-10 08:20:33 +0200

    complete geomap

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import Calendar from "./scenes/calendar/Calendar";
 import FAQ from "./scenes/faq/FAQ";
 import Bar from "./scenes/bar/Bar";
 import Pie from "./scenes/pie/Pie";
+import Geography from "./scenes/geography/Geography";
 // import Line from "./scenes/Line";
 
 function App() {
@@ -36,7 +37,7 @@ function App() {
               <Route path="/bar" element={<Bar />} />
               <Route path="/pie" element={<Pie/>} />
               {/* <Route path="/line" element={<Line />} /> */}
-              {/* <Route path="/geography" element={<Geography/>} /> */}
+              <Route path="/geography" element={<Geography/>} />
             </Routes>
           </main>
         </div>
