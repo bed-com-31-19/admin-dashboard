352a5a9b9087119753b662803aa87488b36808c4
bed-com-31-19 <bed-com-31-19@unima.ac.mw>
2023-01-11 02:49:31 +0200

    recent transactions

--- src/scenes/dashboard/Dashboard.jsx
+++ src/scenes/dashboard/Dashboard.jsx
@@ -159,7 +159,7 @@ const Dashboard = () => {
               </Typography>
               <Typography
                 variant="h4"
-                fontWeight="500"
+                fontWeight="bold"
                 color={colors.greenAccent[400]}
               >
                 MKW 43,263,847
@@ -170,19 +170,79 @@ const Dashboard = () => {
                 <DownloadOutlinedIcon
                   sx={{
                     fontSize: "26px",
-                    color: colors.greenAccent[500]
+                    color: colors.greenAccent[500],
                   }}
                 />
               </IconButton>
             </Box>
           </Box>
 
-          <Box height="250px" ml="20px"
-          >
+          <Box height="250px" ml="20px">
             <LineChart isDashboard={true} />
           </Box>
+        </Box>
+        {/* TRANSACTIONS */}
+        <Box
+          gridColumn="span 4"
+          gridRow="span 2"
+          backgroundColor={colors.primary[400]}
+          overflow="auto"
+        >
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+            borderBottom={`4px solid ${colors.primary[400]}`}
+            colors={colors.grey[100]}
+            p="15px"
+          >
+            <Typography variant="h5" fontWeight="600" color={colors.grey[100]}>
+              Recent transactions
+            </Typography>
+          </Box>
+          {mockTransactions.map((transaction, i) => (
+            <Box
+              key={`${transaction.txId}-${i}`}
+              display="flex"
+              justifyContent="space-between"
+              alignItems="center"
+              borderBottom={`4px solid ${colors.primary[500]}`}
+              p="15px"
+            >
+              <Box>
+                <Typography
+                  variant="h5"
+                  fontWeight="600"
+                  color={colors.greenAccent[500]}
+                >
+                  {transaction.txId}
+                </Typography>
+                <Typography color={colors.grey[100]}>
+                  {transaction.user}
+                </Typography>
+              </Box>
+              <Box color={colors.grey[100]}>{transaction.date}</Box>
+              <Box
+                backgroundColor={colors.primary[400]}
+                p="5px 10px"
+                borderRadius="7px"
+              >
+                {transaction.cost}
+              </Box>
+            </Box>
+          ))}
+        </Box>
 
-          
+        {/* ROW 3 */}
+        <Box
+          gridColumn="span 4"
+          gridRow="span 2"
+          backgroundColor={colors.primary[400]}
+          p="5px"
+        >
+          <Typography>
+            
+          </Typography>
         </Box>
       </Box>
     </Box>
